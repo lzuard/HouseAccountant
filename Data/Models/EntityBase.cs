edad20d21145441229962228484de55dbd817367
Izuard <sasha.20133@gmail.com>
2024-01-02 00:09:27 +0300

    Add data layer

---
+++ Data/Models/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Models
+{
+    internal class EntityBase
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Models
+{
+    internal class User : EntityBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Nickname { get; set; }
+    }
+}
