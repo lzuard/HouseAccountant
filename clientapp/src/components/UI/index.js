028efc2349486d4532aa668739207117a66c2652
Izuard <sasha.20133@gmail.com>
2024-01-06 04:36:55 +0300

    Add content box component

--- clientapp/src/components/UI/index.js
+++ clientapp/src/components/UI/index.js
@@ -1,7 +1,9 @@
 import MainAgGrid from "@/components/UI/MainAgGrid.vue"
 import MainButton from "@/components/UI/MainButton.vue"
+import ContentBox from "@/components/UI/ContentBox.vue"
 
 export default [
     MainAgGrid,
-    MainButton
+    MainButton,
+    ContentBox
 ]
--- clientapp/src/pages/UploadPage.vue
+++ clientapp/src/pages/UploadPage.vue
@@ -1,11 +1,36 @@
 <template>
-    <h1>Upload Page</h1>
+    <content-box
+        headerText="Files to upload" 
+        :isShowContent="true"
+        :isHidable="true"
+        :defaultIsShowContent="true"
+        >
+        <h1>Inside content</h1>
+    </content-box>
+    <content-box 
+        headerText="Preview" 
+        :isShowContent="true"
+        :isHidable="true"
+        >
+        <h1>Inside content</h1>
+    </content-box>
+    <content-box 
+        headerText="Confirm" 
+        :isShowContent="true"
+        :isHidable="true"
+        >
+        <h1>Inside content</h1>
+    </content-box>
 </template>
 
 <script>
+export default{
+
+}
+
 
 </script>
 
 <style scoped>
-
+ 
 </style>
