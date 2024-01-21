94ae0917f3045b4b9c6d8e26b8d893fa5d960b0c
Izuard <sasha.20133@gmail.com>
2024-01-21 05:11:08 +0300

    Implement Core and Tests projects

---
+++ Core/ExcelParser/ExcelParser.cs
@@ -0,0 +1,37 @@
+﻿using ClosedXML.Excel;
+
+namespace Core.ExcelParser
+{
+    public class ExcelParser
+    {
+
+        private string ValidateFile (string fileName) 
+        {
+            return "";
+        }
+
+        public Dictionary<string, List<string>> Parse(string fileName)
+        {
+            var data = new Dictionary<string, List<string>>();
+            using var file = new XLWorkbook(fileName);
+            var sheet = file.Worksheet(1);
+
+            var header = sheet.FirstRowUsed();
+
+            foreach ( var row in sheet.RowsUsed().Skip(1) ) //Iterate rows
+            {
+                for (int i = 2; i<=header.CellCount(); i++) //Iterate columns in row
+                {
+                    var key = header.Cell(i).Value.ToString();
+                    var value = row.Cell(i).Value.ToString();
+
+                    if (!data.ContainsKey(key))
+                        data[key] = new List<string>();
+                    data[key].Add(value);
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ Tests/Core/ExcelParserTest.cs
@@ -0,0 +1,25 @@
+﻿using Core.ExcelParser;
+
+namespace Tests.Core
+{
+    internal class ExcelParserTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Parse()
+        {
+            ExcelParser parser = new ExcelParser();
+            var data = parser.Parse("E:\\Windows Files\\Downloads\\Финансы (1).xlsx");
+            foreach (var key in data.Keys)
+            {
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
